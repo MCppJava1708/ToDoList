35407e26c5853e1dc1d1276fe7d682b273f5684b
Hellcrow <bakamakafoqq@gmail.com>
2018-03-19 21:34:00 +0200

    Revert "2132/19/03/2018"
    This reverts commit 62f4883488f5e42eff632ee77d6c1376250e57f7.

--- script/script.js
+++ script/script.js
@@ -12,53 +12,42 @@ for (i = 0; i < myNodelist.length; i++) {
 // Удаление записи
 var close = document.getElementsByClassName("close");
 var i;
-for (i = 0; i < close.length; i++) 
-{
-  close[i].onclick = function() 
-  {
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
     var div = this.parentElement;
-    div.style.display = "none";//заменить на удаление из БД
+    div.style.display = "none";
   }
 }
 
 // Добавьте символ «checked», когда вы нажимаете на элемент списка.
-var list = document.getElementById("taskUl");
+var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI')  
-  {
-    ev.target.classList.toggle('checked');// добавить замену статуса в БД
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
   }
-});
-//target- проверяет каждый элемент списка на котором у нас висит лиснер
-/*Каждый HTML элемент содержит свойство classList, которое представляет
- из себя объект,с доступнмы для обработки классами.*/
-//classList.toggle - преключить класс (добавить, если его нет, или удалить, если он есть)
-
+}, false);
 
 // Создайте новый элемент списка, нажав кнопку «Добавить»
 function newElement() {
-
-  var li = document.createElement('li');
-  var inputValue = document.getElementById("taskInput").value;
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("taskUl").appendChild(li);
+    document.getElementById("myUL").appendChild(li);
   }
-  document.getElementById("taskInput").value = "";
+  document.getElementById("myInput").value = "";
 
   var span = document.createElement("SPAN");
-  var txt = document.createTextNode("  удалить");
+  var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) 
-  {
-    close[i].onclick = function() 
-    {
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
     }
