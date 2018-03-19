9c42f3b6c07d61f012178cb2a8964c33c52e4ebf
VitaliiYakovlev <v.f.yakovlev@gmail.com>
2018-03-19 23:06:35 +0200

    2303/19/03/18

--- script/scriptLogin.js
+++ script/scriptLogin.js
@@ -1,9 +1,125 @@
 var doc = document;
+var divError = doc.getElementById('divAuth');
+var xmlhttp = new XMLHttpRequest();
 var userNameOrEmail = "";
 var password = "";
+var users = [];
+var count = 1;
+var node;
+
+///////////////////////////////////////////
+/////////// Object Login //////////////////
+///////////////////////////////////////////
+
+function Login (userNameOrEmail, password) {
+	this.userNameOrEmail = userNameOrEmail;
+	this.password = password;
+}
+
+Login.prototype.cheackUser = function(){
+	for (var i = 0; i < users.length; i++) {
+		if (users[i].login === this.userNameOrEmail &&
+			users[i].password === this.password) {
+		doc.location.href = "index.html";
+		} else if (users[i].email === this.userNameOrEmail &&
+			users[i].password === this.password) {
+			doc.location.href = "index.html";
+		}
+		 else {
+			shapeError ();
+		}
+	}	
+}
+
+///////////////////////////////////////////
+/////////// Object User ///////////////////
+///////////////////////////////////////////
+
+function User (id, login, email, password) {
+	this.id = id;
+	this.login = login;
+	this.email = email;
+	this.password = password;
+}
+
+///////////////////////////////////////////
+/////////// Error Shape ///////////////////
+///////////////////////////////////////////
+
+function shapeError () {
+	node = document.createElement('div');
+	node.className = "divErr";
+	node.innerHTML = "Incorrect username or password.";
+	node.position = 'absolute';
+	node.tabIndex = ++count;
+	node.style.color = '#FFFFFFFF';
+	node.style.borderRadius = 10 + 'px';
+	node.style.backgroundColor =  '#FF6570FF';
+	divError.appendChild(node);
+}
+
+///////////////////////////////////////////
+/////////// Server Read Users /////////////
+///////////////////////////////////////////
+
+readPHPCon();
+
+function readPHPCon() {
+	xmlhttp.onreadystatechange = conn;
+	xmlhttp.open("GET", "php/readLogin.php", true);
+	xmlhttp.send();
+}
+
+function conn() {
+	var line;
+	if (xmlhttp.readyState != 4) return;
+
+	if (xmlhttp.status != 200) {
+		console.log(xmlhttp.status + ': ' + xmlhttp.statusText);
+	} else {
+		line = xmlhttp.responseText;
+		var arrLine=[];
+		arrLine = line.split(" ");
+		var count = 0;
+		var id;
+		var login;
+		var email;
+		var pass;
+		for (var i = 0; i < arrLine.length; i++) {
+			switch (count) {
+				case 0:
+				id = arrLine[i];
+				count++;
+				break;
+				case 1:
+				login = arrLine[i];
+				count++;
+				break;
+				case 2:
+				email = arrLine[i];
+				count++;
+				break;
+				case 3:
+				pass = arrLine[i];
+				var user = new User(id, login, email, pass);
+				users.push(user);
+				count = 0;
+				break;
+			}
+		}
+	}
+}
+
+///////////////////////////////////////////
+/////////// VIEW //////////////////////////
+///////////////////////////////////////////
 
 doc.getElementById("btnSignIn").onclick = function () {
-	alert(userNameOrEmail + " " + password);
+	var login = new Login(userNameOrEmail, password);
+	login.cheackUser();
+	if (count > 2) {
+		divError.removeChild(node);
+	} 
 }
 
 doc.getElementById('inputPassword').oninput = function () {
--- script/scriptRegistration.js
+++ script/scriptRegistration.js
@@ -1,9 +1,93 @@
-var login = document.getElementById('login');
+var login      = document.getElementById('login');
+var email      = document.getElementById('email');
+var pass       = document.getElementById('pass');
+var pass2      = document.getElementById('pass2');
+var imgOK1     = document.getElementById('imgOK1');
+var imgCancel1 = document.getElementById('imgCancel1');
+var imgOK2     = document.getElementById('imgOK2');
+var imgCancel2 = document.getElementById('imgCancel2');
 
-login.onblur = function () {
+<<<<<<< HEAD
+
+pass.onblur = function () {
+	strpass  = pass.value;
+	strpass2 = pass2.value;
+	str = passwordCheck (strpass, strpass2);
+	if (str == "OK")
+	{
+		imgCancel1.style.display = 'none';
+		imgCancel1.style.visible = false;
+		imgOK1.style.display     = 'inline';
+		imgOK1.style.visible     = true;
+	}
+	else
+	{
+		imgOK1.style.display     = 'none';
+		imgOK1.style.visible     = false;
+		imgCancel1.style.display = 'inline';
+		imgCancel1.style.visible = true;
+//		imgCancel2.data-tooltip  = "str";
+	}
+};
+
+pass2.onblur = function () {
+	strpass  = pass.value;
+	strpass2 = pass2.value;
+	str = passwordCheck (strpass2, strpass);
+	if (str == "OK")
+	{
+		imgCancel2.style.display = 'none';
+		imgCancel2.style.visible = false;
+		imgOK2.style.display     = 'inline';
+		imgOK2.style.visible     = true;
+	}
+	else
+	{
+		imgOK2.style.display     = 'none';
+		imgOK2.style.visible     = false;
+		imgCancel2.style.display = 'inline';
+		imgCancel2.style.visible = true;
+//		imgCancel2.data-tooltip  = "str";
+	}
+};
+
+function passwordCheck (strpass, strpass2) {
+	if (strpass.length < 8)
+	{
+		alert ('The length of password must be at least 8 symbols');
+		return "The length of password must be at least 8 symbols";
+	}
+	if (strpass.search(/\d/) == -1)
+	{
+		alert ("The password must include at least one number");
+		return "The password must include at least one number";
+	}
+	if (strpass.search(/[a-zA-Z]/) == -1)
+	{
+		alert ("The password must include at least one latin letter");
+		return "The password must include at least one latin letter";
+	}
+	if (strpass2 != "")
+	{
+		if (strpass2 != strpass)
+		{
+			alert ("Password 1 must equals to password 2");
+			return "Password 1 must equals to password 2";
+		}
+		else
+		{
+			return "OK";
+		}
+	}
+	else
+		return "OK";
+
+=======
+login.onblur = function {
 	alert ('Checking');
-};
+}
 
-login.onfocus = function () {
-	login.value = "value";
-};
+login.onfocus = function {
+	alert ('Checking');
+>>>>>>> 58daaeb6900809d4b91ba0fc5e11771c4e86e883
+}
