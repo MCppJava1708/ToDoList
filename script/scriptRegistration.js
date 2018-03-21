2bd4022d2b8d591f2fe3155098d7553cc4ce6f1a
VitaliiYakovlev <v.f.yakovlev@gmail.com>
2018-03-21 23:48:42 +0200

    2348/21/03/18

--- script/scriptRegistration.js
+++ script/scriptRegistration.js
@@ -6,12 +6,25 @@ var imgOK1     = document.getElementById('imgOK1');
 var imgCancel1 = document.getElementById('imgCancel1');
 var imgOK2     = document.getElementById('imgOK2');
 var imgCancel2 = document.getElementById('imgCancel2');
+var pass2Error = document.getElementById('pass2Error');
+var xhr = new XMLHttpRequest();
 
-pass.onblur = function () {
+login.onblur = function () 
+{
+	xhr.onreadystatechange = receive;
+	var send = "&login=" + login.value;
+	xhr.onreadystatechange = receive;
+	xhr.open ('POST', "php/registerCheck.php", true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send(send);	
+}
+
+pass.onblur = function () 
+{
 	strpass  = pass.value;
 	strpass2 = pass2.value;
 	str = passwordCheck (strpass, strpass2);
-	if (str == "OK")
+	if (str == true)
 	{
 		imgCancel1.style.display = 'none';
 		imgCancel1.style.visible = false;
@@ -24,7 +37,6 @@ pass.onblur = function () {
 		imgOK1.style.visible     = false;
 		imgCancel1.style.display = 'inline';
 		imgCancel1.style.visible = true;
-		imgCancel1.setAttribute('data-tooltip', str);
 	}
 };
 
@@ -32,7 +44,7 @@ pass2.onblur = function () {
 	strpass  = pass.value;
 	strpass2 = pass2.value;
 	str = passwordCheck (strpass2, strpass);
-	if (str == "OK")
+	if (str == true)
 	{
 		imgCancel2.style.display = 'none';
 		imgCancel2.style.visible = false;
@@ -45,38 +57,52 @@ pass2.onblur = function () {
 		imgOK2.style.visible     = false;
 		imgCancel2.style.display = 'inline';
 		imgCancel2.style.visible = true;
-		imgCancel2.setAttribute('data-tooltip', str);
 	}
 };
 
 function passwordCheck (strpass, strpass2) {
 	if (strpass.length < 8)
-	{
-		alert ('The length of password must be at least 8 symbols');
-		return "The length of password must be at least 8 symbols";
-	}
+		return false;
+	
 	if (strpass.search(/\d/) == -1)
-	{
-		alert ("The password must include at least one number");
-		return "The password must include at least one number";
-	}
+		return false;
+	
 	if (strpass.search(/[a-zA-Z]/) == -1)
+		return false;
+	
+	if (strpass2 != "" && strpass2 != strpass)
+		return false;
+				
+	return true;
+};
+
+function receive () 
+{
+	if (xhr.readyState != 4) return;
+				
+	if (xhr.status != 200) 
 	{
-		alert ("The password must include at least one latin letter");
-		return "The password must include at least one latin letter";
-	}
-	if (strpass2 != "")
+		alert ('Connection error');
+	} 
+	else 
 	{
-		if (strpass2 != strpass)
+		var res = xhr.responseText;
+		alert (res);
+		if (res == 0) 
 		{
-			alert ("Password 1 must equals to password 2");
-			return "Password 1 must equals to password 2";
+			imgCancel1.style.display = 'none';
+			imgCancel1.style.visible = false;
+			imgOK1.style.display     = 'inline';
+			imgOK1.style.visible     = true;
+		} 
+		else if (res == 1)
+		{
+			imgOK1.style.display     = 'none';
+			imgOK1.style.visible     = false;
+			imgCancel1.style.display = 'inline';
+			imgCancel1.style.visible = true;
 		}
 		else
-		{
-			return "OK";
-		}
+			alert ("Reqest error");
 	}
-	else
-		return "OK";
-};
+}
