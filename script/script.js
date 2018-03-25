da2efa9d37d9f5bbb8d7933dbd38f25b7a88c0de
Rush753 <31591169+Rush753@users.noreply.github.com>
2018-03-25 16:39:11 +0200

    1638/25/03/2018

--- script/script.js
+++ script/script.js
@@ -1,63 +1,14 @@
 var tasks = [];
 var xmlhttp = new XMLHttpRequest();
 var taskUl = document.getElementById('taskUl');
-var count = 1;
+var taskTextInput = document.getElementById('taskInput');
 
 var id;
 var name = "H";
-var task = document.getElementById('taskInput');
+var taskText;
 var statusTask = 1;
 
-/////////////////////////////////////////////////////////
-//Добавить таск в HTML
-/////////////////////////////////////////////////////////
 
-function createTask(task){
-  var newLi = document.createElement('li');  
-  var text = task.task ;
-  var t = document.createTextNode(text);
-  newLi.appendChild(t);
-  
-  if(task.statusTask == 0)
-  {
-    newLi.classList.add('checked')
-  }
-  taskUl.appendChild(newLi);
-
-  //Создаем кнопку "close" и добавляем ее в <li>
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode(" \u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  newLi.appendChild(span);
-}
-
-/////////////////////////////////////////////////////////
-//Добавьте символ «checked» (перечеркнуть строку), когда вы нажимаете на 
-//элемент списка) и удаление при нажатии на delete
-/////////////////////////////////////////////////////////
-
-var list = document.getElementById("taskUl");
-list.addEventListener('click', function(ev) 
-{
-  if (ev.target.tagName === 'LI')  
-  {
-    //ev.target.classList.toggle('checked');
-    var str = ev.target.parentElement.innerText;
-    task = str.substring(0,str.indexOf(" "));
-    UpdateStatusTaskInDB(task);
-  }
-  else if (ev.target.className === "close")
-  {
-    var str = ev.target.parentElement.innerText;
-    task = str.substring(0,str.indexOf(" "));
-    dellFromDbTask(task);
-  }
-});
-//target- проверяет каждый элемент списка на котором у нас висит лиснер
-/*Каждый HTML элемент содержит свойство classList, которое представляет
-из себя объект,с доступнмы для обработки классами.*/
-//classList.toggle - преключить класс (добавить, если его нет, или удалить, если он есть)
 
 /////////////////////////////////////////////////////////
 // Создайте новый элемент списка, нажав кнопку «Добавить»
@@ -66,9 +17,76 @@ list.addEventListener('click', function(ev)
 document.getElementById("addBton").onclick = function newElement() 
 {
   xmlhttp.onreadystatechange = conn;
-  str = "'"+ name + "','"+ task.value + "'," + 1;
+  str = "'"+ name + "','"+ taskTextInput.value + "'," + 1;
   console.log(str);
   xmlhttp.open("GET", "php/createTask.php?&str="+str, true);
+  xmlhttp.send();
+
+  taskTextInput.value="";
+  
+
+  function conn() 
+  { 
+    if (xmlhttp.readyState != 4) return;
+    if (xmlhttp.status != 200) 
+    {
+      alert(xhr.status + ': ' + xhr.statusText);
+    } 
+    else 
+    {
+      var line = xmlhttp.responseText;
+      //alert(line)
+      readDb(name);
+    }
+  }
+}
+
+/////////////////////////////////////////////////////////
+// Обновить statusTask из BD
+/////////////////////////////////////////////////////////
+
+//Добавьте символ «checked» (перечеркнуть строку), когда вы нажимаете на 
+//элемент списка) и удаление при нажатии на delete
+
+var list = document.getElementById("taskUl");
+list.addEventListener('click', function(ev) 
+{
+  if (ev.target.tagName === 'LI')  
+  {
+    //ev.target.classList.toggle('checked');
+    var str = ev.target.parentElement.innerText;
+    taskText = str.substring(0,str.indexOf(" "));
+    GetStatusTaskInDB(taskText);
+  }
+  else if (ev.target.className === "close")
+  {
+    var str = ev.target.parentElement.innerText;
+    taskText = str.substring(0,str.indexOf(" "));
+    dellFromDbTask(taskText);
+  }
+});
+//target- проверяет каждый элемент списка на котором у нас висит лиснер
+/*Каждый HTML элемент содержит свойство classList, которое представляет
+из себя объект,с доступнмы для обработки классами.*/
+//classList.toggle - преключить класс (добавить, если его нет, или удалить, если он есть)
+
+
+
+function UpdateStatusTaskInDB(taskText) 
+{
+  GetStatusTaskInDB(taskText);
+  //alert(statusTask);
+  if(statusTask == 1)
+  {
+    statusTask = 0;
+  }
+  else
+  {
+    statusTask = 1;
+  }
+
+  xmlhttp.onreadystatechange = conn;
+  xmlhttp.open("GET", "php/UpdateStatusTask.php?texttask=" + "'" +taskText+ "'" + "&statusTask=" + statusTask, true);
   xmlhttp.send();
 
   function conn() 
@@ -82,18 +100,15 @@ document.getElementById("addBton").onclick = function newElement()
     {
       var line = xmlhttp.responseText;
       //alert(line)
+      //readDb(name);
     }
   }
 }
 
-/////////////////////////////////////////////////////////
-// Обновить statusTask из BD
-/////////////////////////////////////////////////////////
-
-function GetStatusTaskInDB(task) 
+function GetStatusTaskInDB(taskText) 
 {
   xmlhttp.onreadystatechange = conn;
-  xmlhttp.open("GET", "php/getStatusTask.php?&texttask="+"'"+task+"'", true);
+  xmlhttp.open("GET", "php/getStatusTask.php?&texttask="+"'"+taskText+"'", true);
   xmlhttp.send();
 
   function conn() 
@@ -106,40 +121,8 @@ function GetStatusTaskInDB(task)
     else 
     {
       var line = xmlhttp.responseText;
-      alert("statusTask: "+line)
+      alert("statusTask was: "+line)
       statusTask = line;
-    }
-  }
-}
-
-function UpdateStatusTaskInDB(task) 
-{
-  GetStatusTaskInDB(task);
-  //alert(statusTask);
-  if(statusTask == 1)
-  {
-    statusTask = 0;
-  }
-  else
-  {
-    statusTask=1;
-  }
-
-  xmlhttp.onreadystatechange = conn;
-  xmlhttp.open("GET", "php/UpdateStatusTask.php?texttask=" + "'" +task+ "'" + "&statusTask=" + statusTask, true);
-  xmlhttp.send();
-
-  function conn() 
-  { 
-    if (xmlhttp.readyState != 4) return;
-    if (xmlhttp.status != 200) 
-    {
-      alert(xhr.status + ': ' + xhr.statusText);
-    } 
-    else 
-    {
-      var line = xmlhttp.responseText;
-      alert(line)
     }
   }
 }
@@ -148,11 +131,11 @@ function UpdateStatusTaskInDB(task)
 // удалить таск из BD
 /////////////////////////////////////////////////////////
 
-function dellFromDbTask(task) 
+function dellFromDbTask(taskText) 
 {
 
   xmlhttp.onreadystatechange = conn;
-  xmlhttp.open("GET", "php/dellTask.php?&texttask="+"'"+task+"'", true);
+  xmlhttp.open("GET", "php/dellTask.php?&texttask="+"'"+taskText+"'", true);
   xmlhttp.send();
 
   function conn() 
@@ -166,7 +149,8 @@ function dellFromDbTask(task)
     else 
     {
       var line = xmlhttp.responseText;
-      alert(line)
+      //alert(line)
+      readDb(name);
     }
   }
 }
@@ -175,19 +159,21 @@ function dellFromDbTask(task)
 /////////// Object Task ///////////////////
 ///////////////////////////////////////////
 
-function Task (id, name, task, statusTask) {
+function Task (id, name, taskText, statusTask) {
   this.id = id;
   this.name = name;
-  this.task = task;
+  this.taskText = taskText;
   this.statusTask = statusTask;
 }
 
 ///////////////////////////////////////////
-/////////// Server Read Task /////////////
+///////////  Read Task из бд /////////////
 ///////////////////////////////////////////
 
 function readDb(login) 
 {
+  document.getElementById('taskUl').innerHTML = '';
+
   xmlhttp.onreadystatechange = conn;
   xmlhttp.open("GET", "php/readTask.php?name=" + login, true);
   xmlhttp.send();
@@ -219,12 +205,12 @@ function conn()
         count++;
         break;
         case 2:
-        task = arrLine[i];
+        taskText = arrLine[i];
         count++;
         break;
         case 3:
         statusTask = arrLine[i];
-        var task = new Task(id, name, task, statusTask);
+        var task = new Task(id, name, taskText, statusTask);
         tasks.push(task);
         createTask(task);
         console.log(task);
@@ -233,6 +219,28 @@ function conn()
       }
     }
   }
+}
+
+//Добавить таск в HTML
+
+function createTask(task){
+  var newLi = document.createElement('li');  
+  var taskText = task.taskText ;
+  var t = document.createTextNode(taskText);
+  newLi.appendChild(t);
+  
+  if(task.statusTask == 0)
+  {
+    newLi.classList.add('checked')
+  }
+  taskUl.appendChild(newLi);
+
+  //Создаем кнопку "close" и добавляем ее в <li>
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode(" \u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  newLi.appendChild(span);
 }
 
 /////////////////////////////////////////////////////////
